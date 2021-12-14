0554f9b034b0169acb545bfb6020df082c7c0594
maruo-777 <thegashigashi@icloud.com>
2021-12-14 23:32:39 +0900

    12/14進捗分更新

--- TwitterAPI_lesson.py
+++ TwitterAPI_lesson.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 import tweepy
 
 # 認証に必要なキーとトークン
-API_KEY = 'your_api_key'
-API_SECRET = 'your_api_secret'
-ACCESS_TOKEN = 'your_access_token'
-ACCESS_TOKEN_SECRET = 'your_access_token_secret'
+API_KEY = 'ORQaivdLITqo3qTvxM4Y6h4Rl'
+API_SECRET = 'qmQuNTIQ9xKCjTQ2adLZQvsDK8kP5A9obwaAd77kFxOUagWg0m'
+ACCESS_TOKEN = '4803016783-Sq6dcTCsfp8xLUycb5YbAxjLXlo7eN1cbAPhi94'
+ACCESS_TOKEN_SECRET = 'HnWreMf51mG5PdoXaN2eWiWeIXFewILvwMYDj16F8wOLP'
 
 # APIの認証
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
---
+++ helloworld.py
@@ -0,0 +1,6 @@
+import sys
+import pprint
+pprint.pprint(sys.path)
+
+
+/Users/maruo/Library/Python/3.8/lib/python/site-packages
